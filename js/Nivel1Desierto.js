e40c9ce9c982afbfc08a4617a53a3b0bccd33f40
Majoo02 <you@example.com>
2019-09-30 16:24:50 -0300

    Genero la colision.

--- js/Nivel1Desierto.js
+++ js/Nivel1Desierto.js
@@ -1,6 +1,6 @@
 var canvas=document.getElementById('ProyectoEnduro'); //Conecta el html con el js
 var contexto=canvas.getContext('2d'); // Canvas cn el contexto en 2d
-var score=0;
+var score=0; //Acumulador puntos
 
 window.addEventListener("keydown", controles, false); // Cuando la tecla se mantiene apretada, llama a la función avanzar y le asigna verdadero
 
@@ -12,9 +12,9 @@ function controles(evento){ //controles tiene evento como parametro
     //      x+=10;
     //  }
     if (evento.keyCode==87){ //Si la tecla apretada es 87, avanza en y
-        if(!saltando) y-=130; //Si saltando es verdadero sube 50 en y
+        if(!saltando) yMoto-=130; //Si saltando es verdadero sube 50 en y
         saltando=true; //Es para que al mantener apretada la tecla no siga subiendo y se pase del límite dado para saltar.
-        score+=2;
+        score+=2; //Cuando salta suma 2 puntos
     }
 }
 
@@ -49,11 +49,11 @@ listenKeyright = function (e) {
 var moto = new Image(); //A la variable moto se le asignará una imagen
 saltando= false;
 
-x=10, y=255, xc=0; //moto se inicializa en esa posición
+x=10, yMoto=255, xc=0, muerto= false; //x general, y moto, x cactus
 
 moto.src='../css/imagenes/PersonajeMoto.png' //A moto se le asigna la foto PersonajeMoto
 function dibujarMoto(){
-    contexto.drawImage(moto, 40,y,70,70);
+    contexto.drawImage(moto, 40,yMoto,70,70);
 }
 
 var fondo = new Image(); //A la variable fondo se le asignará una imágen
@@ -79,29 +79,45 @@ function MovCactus(){
 var ancho=640;
 var alto= 380;
 
+function choque(){
+    if (xc>=40 && xc<=70){
+        if (yMoto>= 60){
+            muerto= true;
+            console.log(muerto);
+        }
+    }
+}
+
 function borrarCanvas(){
     canvas.width= ancho;
     canvas.height= alto;
 }
+
+///////////////////////////
+//////BUCLE PRINCIPAL//////
+//////////////////////////
+
 setInterval(actualizarJuego,1000/100);
 
 function actualizarJuego(){ //La función actualizarJuego hará:
     borrarCanvas();
-    y +=2; //Velocidad en que cae el personaje
-    if (y>255){ //Para que vuelva al piso
-        y=255;
-        saltando=false;
+    if (muerto!=true){
+        yMoto +=2; //Velocidad en que cae el personaje
+        if (yMoto>255){ //Para que vuelva al piso
+            yMoto=255;
+            saltando=false;
+        }
+        //if (x>=575){ //Para que no se ppase del límite de la pantalla
+        //    x=575;
+        //}
+        //if (x<=0){//Para que no se ppase del límite de la pantalla
+        //    x=0;
+        //}
+        dibujarFondo(); // Dibuja el fondo en la posicio 00 con un ancho de 640 y un largo de 380
+        choque();
+        MovCactus();
+        dibujarCactus();
+        dibujarMoto();//Dibuja la moto en laposición en la que esté x, y=255 y el ancho y largo=70
+        contexto.fillText("Puntos: "+score, 5, 20);
     }
-    //if (x>=575){ //Para que no se ppase del límite de la pantalla
-    //    x=575;
-    //}
-    //if (x<=0){//Para que no se ppase del límite de la pantalla
-    //    x=0;
-    //}
-    dibujarFondo(); // Dibuja el fondo en la posicio 00 con un ancho de 640 y un largo de 380
-    MovCactus();
-    dibujarCactus();
-    dibujarMoto();//Dibuja la moto en laposición en la que esté x, y=255 y el ancho y largo=70
-    contexto.fillText("Puntos: "+score, 5, 20);
-    
 }
